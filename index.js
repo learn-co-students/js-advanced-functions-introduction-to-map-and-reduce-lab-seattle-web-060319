3c7c7203780237c403a4b6cb9480a29f56dd5f8e
Jason Leach <jlspursfan@gmail.com>
2019-08-22 17:53:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(arr) {
+  let newArr = [];
+  let i;
+  for(i = 0; i < arr.length; i++){
+    newArr.push(-(arr[i]))
+  }
+  return newArr;
+}
+
+function mapToNoChange(arr) {
+  let i;
+  let newArr = [];
+  for(i = 0; i < arr.length; i++){
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+function mapToDouble(arr) {
+  let i;
+  let newArr = [];
+  for(i = 0; i < arr.length; i++){
+    newArr.push(arr[i]*2)
+  }
+  return newArr;
+}
+
+function mapToSquare(arr) {
+  let i;
+  let newArr = [];
+  for(i = 0; i < arr.length; i++){
+    newArr.push(arr[i]**2)
+  }
+  return newArr;
+}
+
+function reduceToTotal(arr, start=0) {
+  let i;
+  for(i = 0; i < arr.length; i++){
+    start += arr[i]
+  }
+  return start
+}
+
+function reduceToAllTrue(arr) {
+  let i;
+  for(i = 0; i < arr.length; i++){
+    if(!arr[i]){return false}
+  }
+  return true
+}
+
+function reduceToAnyTrue(arr) {
+  let i;
+  for(i = 0; i < arr.length; i++){
+    if(arr[i]){return true}
+  }
+  return false
+}
